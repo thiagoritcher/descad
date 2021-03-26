59ca298162ea51d4673af774a4c95550d28f6ba7
Thiago Ritcher <thiagoritcher@gmail.com>
2021-03-26 10:40:45 -0300

    Argumentos de programa e help

--- src/main/java/br/com/ritcher/descad/App.java
+++ src/main/java/br/com/ritcher/descad/App.java
@@ -3,14 +3,12 @@ package br.com.ritcher.descad;
 import com.jsevy.jdxf.DXFDocument;
 import com.jsevy.jdxf.DXFGraphics;
 
-import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
 /**
  * Hello world!
@@ -21,14 +19,81 @@ public class App {
 
 	private DXFDocument dxfDocument;
 	private DXFGraphics dxfGraphics;
-	private final String file = "C:\\Users\\talvares\\Downloads\\descad\\out.dxf";
-	private final String infile = "C:\\Users\\talvares\\Downloads\\descad\\input.csv";
+	private String file;
+	private String infile;
 
 	public static void main(String[] args) throws IOException {
 		App ap = new App();
+		for (int i = 0; i < args.length; i++) {
+			switch (args[i]){
+				case "--input":
+				case "-i":
+					ap.infile = args[i+1];
+					i++;
+					break;
+				case "--output":
+					ap.file = args[i+1];
+					i++;
+					break;
+				case "--help":
+				default:
+					print_help(null);
+					break;
+			}
+		}
+
+		if(ap.infile == null || ap.file == null){
+			print_help("--input e --output devem ser especificados.");
+			return;
+		}
+
 		ap.readInput();
 		ap.process();
 		ap.write();
+	}
+
+	private static void print_help(String s) {
+		StringBuilder sb = new StringBuilder();
+		if(s != null){
+			sb.append("Erro: ").append(s).append('\n');
+			sb.append("---------------------------------").append('\n');
+		}
+		sb.append("Descad - CAD descritivo").append('\n');
+		sb.append("Desenho a partir de informações em arquivo csv").append('\n');
+		sb.append("descad --input,-i <input_file> --output,-o <dxf_file>").append('\n');
+		sb.append("---------------------------------").append('\n');
+		sb.append("Exemplo de entrada:").append('\n');
+		sb.append("Modules").append('\n');
+		sb.append("ID	W	H	Commands").append('\n');
+		sb.append("m1	80	15	pl	-40	-7.5	40	-7.5	25	7.5").append('\n');
+		sb.append("m2	5	5	pl	-2.5	-2.5	2.5	-2.5	0	5").append('\n');
+		sb.append('\n');
+		sb.append("Design").append('\n');
+		sb.append("ID	W	H	Loc	Module	dx	dy").append('\n');
+		sb.append("	1	150	150	0	R	0	0").append('\n');
+		sb.append("	2	80	150	O1	R").append('\n');
+		sb.append("	3	20	150	O2	R").append('\n');
+		sb.append("4	80	150	L1	R").append('\n');
+		sb.append("5	20	150	L4	R").append('\n');
+		sb.append("6	150	80	S1	R").append('\n');
+		sb.append("7	150	20	S6	R").append('\n');
+		sb.append("8	150	80	N1	R").append('\n');
+		sb.append("9	153	153	N8	R").append('\n');
+		sb.append("10	20	153	O9	R").append('\n');
+		sb.append("11	15	153	O10	R").append('\n');
+		sb.append("12	20	153	L9	R").append('\n');
+		sb.append("13	15	153	L12	R").append('\n');
+		sb.append("14	153	20	N9	R").append('\n');
+		sb.append("15	153	15	N14	R").append('\n');
+		sb.append("16	0	0	N2	m1").append('\n');
+		sb.append("17	0	0	S2	m1").append('\n');
+		sb.append("18	0	0	N4	m1").append('\n');
+		sb.append("19	0	0	S4	m1").append('\n');
+		sb.append("20	35	20	O14	R").append('\n');
+		sb.append("21	35	20	L14	R").append('\n');
+		sb.append("22	0	0	S21	m2	2.5	0").append('\n');
+		sb.append("23	0	0	S20	m2	-2.5	0").append('\n');
+		System.out.println(sb.toString());
 	}
 
 	void readInput() throws IOException {
